69aede5ade81692f8eb808a4bd880ccb3d5ca35e
Mario Lasić <mario.lasic@outlook.com>
2023-10-17 17:01:03 +0200

    Add function converter(That converts string to morse code)

--- main.py
+++ main.py
@@ -41,3 +41,18 @@ morse_code_dict = {
 # Getting user input
 user_input = input("Write something you want to encrypt: ")
 
+# Function that convert normal string to morse code
+def converter(input):
+    encrypted_input = ""
+    for char in input:
+        if char == " ":
+            encrypted_input += "       "
+        elif char.upper() in morse_code_dict.keys():
+            encrypted_input += morse_code_dict[char.upper()]
+            encrypted_input += "  " 
+        else:
+            encrypted_input += char
+
+    return encrypted_input
+
+print(converter(user_input))
